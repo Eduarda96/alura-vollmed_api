b2a5b1465ed35483968a9d87d2746e95b5089852
Maria Eduarda de Abreu Bortoletti <eduarda.bortoletti@dbserver.com.br>
2024-08-26 18:07:33 -0300

    lista medicos

--- src/main/java/com/bortoletti/api/controller/MedicoController.java
+++ src/main/java/com/bortoletti/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package com.bortoletti.api.controller;
 
 import com.bortoletti.api.medico.DadosCadastroMedico;
+import com.bortoletti.api.medico.DadosListagemMedico;
 import com.bortoletti.api.medico.Medico;
 import com.bortoletti.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+       return repository.findAll().stream().map(DadosListagemMedico::new).toList(); //converte lista de medicos para DadosListamMedico
     }
 }
