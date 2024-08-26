2371060b94e7eccb91d5b2386ae6917c4d2ad7f7
Maria Eduarda de Abreu Bortoletti <eduarda.bortoletti@dbserver.com.br>
2024-08-26 17:40:27 -0300

    cadastra pacientes

--- src/main/java/com/bortoletti/api/controller/PacienteController.java
+++ src/main/java/com/bortoletti/api/controller/PacienteController.java
@@ -1,6 +1,11 @@
 package com.bortoletti.api.controller;
 
 import com.bortoletti.api.paciente.DadosPaciente;
+import com.bortoletti.api.paciente.Paciente;
+import com.bortoletti.api.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("paciente")
 public class PacienteController {
+    @Autowired
+    PacienteRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosPaciente dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosPaciente dados){
+        repository.save(new Paciente(dados));
     }
 }
