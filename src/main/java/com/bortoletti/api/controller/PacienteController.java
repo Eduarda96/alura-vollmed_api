fc4d7cc4eeef4f1fa8bc46c62f52a1305d94752e
Eduarda Bortoletti <eduarda.bortoletti@dbserver.com.br>
2024-08-27 10:24:16 -0300

    lista pacientes

--- src/main/java/com/bortoletti/api/controller/PacienteController.java
+++ src/main/java/com/bortoletti/api/controller/PacienteController.java
@@ -1,15 +1,17 @@
 package com.bortoletti.api.controller;
 
+import com.bortoletti.api.paciente.DadosListagemPaciente;
 import com.bortoletti.api.paciente.DadosPaciente;
 import com.bortoletti.api.paciente.Paciente;
 import com.bortoletti.api.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("paciente")
@@ -21,5 +23,10 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosPaciente dados){
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"})  Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new).toList();
     }
 }
---
+++ src/main/java/com/bortoletti/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,8 @@
+package com.bortoletti.api.paciente;
+
+public record DadosListagemPaciente(String nome, String email) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail());
+    }
+}
